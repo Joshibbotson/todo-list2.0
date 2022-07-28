c236efc4309a221b0982daef0fe47fc257261bfe
Joshua Ibbotson <joshua_ibbotson@hotmail.com>
2022-07-28 11:04:15 +0100

    fixing issues to upload to netlify to test development

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,9 @@
 module.exports = {
     entry: "./src/index.js",
+    // output: {
+    //     filename: "main.js",
+    //     path: path.resolve(__dirname, "dist"),
+    // },
     mode: "development",
     module: {
         rules: [
